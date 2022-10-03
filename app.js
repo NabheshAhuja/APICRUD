e9236466771f1761afb6c2dace140a8d076db0af
Nabhesh Ahuja <Nabhesh.a.insnapsys.com>
2022-10-03 10:28:22 +0530

    New Branch switch

--- app.js
+++ app.js
@@ -1,64 +1,28 @@
-const express = require("express");
-const http = require("http");
-const StaffController = require("./controller/controller");
+// const express = require("express");
+// const http = require("http");
+// const StaffController = require("./controller/controller");
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-app.post("/staff", async (req, res) => {
-  await StaffController.insertStaff(req, res);
-});
-// app.put("/update/:id", async (req, res) => {
-//   let upid = req.params.id;
-//   let upname = req.body.name;
-//   let updes = req.body.designation;
-//   let upsal = req.body.salary;
-//   let upjd = req.body.joined_date;
-//   monmodel.findOneAndUpdate(
-//     { id: upid },
-//     {
-//       $set: {
-//         name: upname,
-//         designation: updes,
-//         salary: upsal,
-//         joined_date: upjd,
-//       },
-//     },
-//     { new: true },
-//     (err, data) => {
-//       if (err) {
-//         res.send("ERROR");
-//       } else {
-//         if (data == null) {
-//           res.send("Not found id");
-//         } else {
-//           res.send(data);
-//         }
-//       }
-//     },
-//   );
+// app.post("/staff", async (req, res) => {
+//   await StaffController.insertStaff(req, res);
 // });
-// // app.delete("/delete", (req, res) => {
-// //   res.send("delete staff by id");
-// // });
-// app.get("/staff/:id", function (req, res) {
-//   fetchid = req.params.id;
-//   monmodel.find({ id: fetchid }, function (err, val) {
-//     res.send(val);
-//   });
+// app.put("/update/:id", async(req, res) => {
+//   await StaffController.updateById(req, res);
 // });
 
-// app.delete("/delete/:id", function (req, res) {
-//   let delid = req.params.id;
-//   monmodel.findOneAndDelete({ id: delid }, function (err, val) {
-//     if (val == null) {
-//       res.send("Not found");
-//     } else {
-//       res.send(val);
-//     }
-//   });
+// app.get("/staff/:id", async(req, res) => {
+//   await StaffController.getById(req, res);
 // });
-port = "80";
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.delete("/delete/:id", async(req, res) => {
+//   await StaffController.deleteById(req, res);
+// });
+
+// app.get("/", async(req, res) => {
+//   await StaffController.getAll(req, res);
+// });
+// port = "8000";
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,11 @@
 const { StaffModel } = require("../model/Employee");
-const getAllStaff = (req, callback) => {
-  const data = [
-    { id: "1", name: "shailesh", age: 33 },
-    { id: "2", name: "nabhesh", age: 21 },
-  ];
-  callback(data);
-};
+
+// const page = parseInt(req.query.page);
+// const limit = parseInt(req.query.limit);
+// const skipIndex = (page - 1) * limit;
+
+
+
 const insertStaff = async (req, res) => {
   console.log("insidepostfunction");
   const data = new StaffModel({
@@ -18,5 +18,65 @@ const insertStaff = async (req, res) => {
   const val = await data.save();
   res.json(val);
 };
-// module.exports = { getAllStaff };
-module.exports = { insertStaff };
+
+const updateById = async (req, res) => {
+  let upid = req.params.id;
+  let upname = req.body.name;
+  let updes = req.body.designation;
+  let upsal = req.body.salary;
+  let upjd = req.body.joined_date;
+  StaffModel.findOneAndUpdate(
+    { id: upid },
+    {
+      $set: {
+        name: upname,
+        designation: updes,
+        salary: upsal,
+        joined_date: upjd,
+      },
+    },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        res.send("ERROR");
+      } else {
+        if (data == null) {
+          res.send("Not found id");
+        } else {
+          res.send(data);
+        }
+      }
+    },
+  );
+};
+
+const getById = function (req, res) {
+  fetchid = req.params.id;
+  StaffModel.find({ id: fetchid }, function (err, val) {
+    res.send(val);
+  });
+};
+
+const deleteById = function (req, res) {
+  let delid = req.params.id;
+  StaffModel.findOneAndDelete({ id: delid }, function (err, val) {
+    if (val == null) {
+      res.send("Not found");
+    } else {
+      res.send(val);
+    }
+  });
+};
+
+const getAll = function (req, res) {
+  const page_size = parseInt(req.query.page_size);
+  const page_number = parseInt(req.query.page_number);
+  const SkipIndex = (page_number-1)*page_size;
+  console.log(page_size,page_number, SkipIndex);
+  StaffModel.find(  function (err, val) {
+    res.send(val);
+  }).limit(page_size).skip(SkipIndex);
+};
+
+module.exports = { insertStaff, updateById, getById, deleteById, getAll};
+
